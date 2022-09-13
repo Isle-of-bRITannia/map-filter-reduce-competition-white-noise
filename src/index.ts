d9f614dcd30acd61905cd7bea8bdd20260b719f4
Nathan Wildofsky <nsw7227@rit.edu>
2022-09-13 17:33:47 -0400

    broken

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,42 @@ type Material = {
 const equipment: Equipment[] = botwEquipment;
 const monsters: Monster[] = botwMonsters;
 const materials: Material[] = botwData.data.materials;
+
+
+// Create a data structure of all monster spawn locations
+// Attach to each location all the monster objects that spawn there, but without those Monsters' 'common.locations' and 'id' properties
+
+// Monsters assigned to a location can't be a monster type while missing properties
+// So, monsters could be rebuilt from the ground up 1 property at a time when being assigned
+
+const locations: any = {};
+
+for (let i = 0; i < monsters.length; i++) {
+    for (let j = 0; j < monsters[i].common_locations.length; j++) {
+        if (!locations.hasOwnProperty(monsters[i].common_locations[j])) {
+            locations[`${monsters[i].common_locations[j]}`] = [];
+        }
+    }
+}
+
+for (let key of locations.keys()) {
+    for (let i = 0; i < monsters.length; i++) {
+        for (let j = 0; j < monsters[i].common_locations.length; j++) {
+            if (monsters[i].common_locations[j] === key) {
+                let monster_to_push = Object(monsters[i]);
+                delete monster_to_push.id;
+                delete monster_to_push.common_locations;
+
+
+
+
+
+                key.push(monster_to_push);
+
+
+            }
+        }
+    }
+}
+
+console.log(locations);
